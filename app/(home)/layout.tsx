5a75981b7ec4b89c9286d8cf56058f49c4032239
Maxwell Dagadu <maxwelldagadu02@gmail.com>
2026-09-16 22:38:06 +0000

    redesigned the input field for responsiveness

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/navbar"
+import SearchInputField from "@/components/searchInputfield"
 import type { ReactNode } from "react"
 
 export default function HomeLayout({children}: { children: ReactNode }){
   return (
-    <div className="h-full w-full min-h-full  flex flex-col gap-10">
+    <div className="h-full w-full min-h-full  flex flex-col gap-5">
       <Navbar/>
+      <SearchInputField className={"sm:hidden w-2/3 self-center"}/>
       {children}
     </div>
   )
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,32 +1,27 @@
 import Image from "next/image";
-import { Input } from "@/components/ui/input";
-import { Button } from "@base-ui/react";
-import { Search } from 'lucide-react';
-import { Separator } from "@/components/ui/separator"
 import Link from "next/link";
-
+import SearchInputField from "./searchInputfield";
 
 export default function Navbar(){
   return(
-    <div className="w-full flex justify-between items-center gap-5 p-3">
-      <div className="w-200 flex justify-start items-center">
-        <Image src="/icon.png" alt="crytogram logo" width={90} height={90}/>
-        <h1 className="text-l font-mono md:text-xl lg:text-2xl text-accent-yellow">CRYTOGRAM</h1>
+    <div className="relative w-full flex justify-between items-center gap-5">
+      <div className="w-100 lg:w-200 flex justify-start items-center">
+        <Image 
+          src="/icon.png" 
+          alt="crytogram logo" 
+          width={90} height={90} 
+          className="size-10 sm:size-13 md:size-12 lg:size-14 xl:size-15"/>
+        <h1 className="text-l font-mono md:text-xl lg:text-2xl text-accent-yellow">CRYPTOGRAM</h1>
       </div>
       
-      <div className="flex justify-center gap-1 items-center w-full border-2 rounded-4xl bg-light-dark/5">
-        <button className="hover:cursor-pointer">
-          <Search  className="text-custom-gray ml-1 "/>
-        </button>
-        <Input className="w-full border-0 rounded-none outline-0 h-10 text-muted-foreground font-mono placeholder:font-mono" placeholder="Search"/>
-      </div>
-     
-      <div  className="w-200 flex justify-end items-center rounded-4xl">
+      <SearchInputField className={"hidden sm:flex"}/>
+      
+      <div  className="w-100 lg:w-200 flex justify-end items-center rounded-4xl">
         <div className="flex justify-end items-center gap-4 w-full font-mono font-medium">
-          <Link href="/signin" className="border py-2 px-3.5 rounded-4xl cursor-pointer text-accent-yellow bg-light-dark/20 hover:text-black hover:bg-accent-yellow">
+          <Link href="/signin" className="btn">
             Sign In
           </Link>
-          <Link href="/signup" className="border py-2 px-3.5 rounded-4xl cursor-pointer text-accent-yellow bg-light-dark/20 hover:text-black hover:bg-accent-yellow">
+          <Link href="/signup" className="btn">
             Sign Up
           </Link>
         </div>
